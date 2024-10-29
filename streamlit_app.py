b4e68bdcbb74496f5df8ab9cc33b3e82ada87da9
lisa.ngg <lisa03062007@gmail.com>
2024-10-29 14:47:33 -0400

    add two graphing function and getting fundamental data from Ticker object

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,11 +5,12 @@ import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
 import seaborn as sns
+import plotly.graph_objects as go
+
+
+
 from datetime import datetime, timedelta
 
-
-
-st.title("Stock Market Dashboard")
 
 # decorator @st.cache_data and @st.cache_resource 
 #  store the results of expensive function calls and return the cached result when the same inputs occur again
@@ -27,13 +28,13 @@ def load_ticker(ticker='MSFT'):
     return yf.Ticker(ticker)
 
 @st.cache_data
-def load_data(start_date = '2000-01-01', end_date = datetime.today().strftime('%Y-%m-%d'), ticker='GOOGL'):
+def load_data(start_date='2005-01-01', end_date=datetime.today(), symbol='GOOGL'):
     # docStrings for a function is created with triple-quotation marks
     ''' download data for a specific ticker from yfinance library
     
     Args:
-        start_date: represent the starting date in %Y-%m-%d format, string
-        end_date: represent the ending date in %Y-%m-%d format, string
+        start_date: represent the starting date, string
+        end_date: represent the ending date, datetime object
         ticker: ticker symbol for a stock, string
     
     Returns:
@@ -42,35 +43,86 @@ def load_data(start_date = '2000-01-01', end_date = datetime.today().strftime('%
 
     # a function download data from yfinance
     
-    data = yf.download(ticker,start_date, end_date)
+    data = yf.download(symbol, start_date, end_date)
     
     return data
 
-data = load_data()
-st.write(data)
+@st.cache_data
+def get_tickers_name():
+    return pd.read_csv("nasdaq_screener.csv")["Symbol"]
+        
 
-# ticker = load_ticker()
+# visualizing functions
+def display_line_graph(symbol, data):
+    st.subheader(F'{symbol} Closing Price')
+    st.line_chart(data['Adj Close'], x_label='Date', y_label='Closing Price')
+
+def display_candle_stick(symbol, data):
+    
+    # Plotting the candlestick chart with Plotly
+    fig = go.Figure(data=[go.Candlestick(x=data.index,
+                                        open=data['Open'][symbol],
+                                        high=data['High'][symbol],
+                                        low=data['Low'][symbol],
+                                        close=data['Adj Close'][symbol])])
+
+    fig.update_layout(title=f'{symbol} Candlestick Chart', xaxis_title='Date', yaxis_title='Price')
+
+    # Display the figure
+    st.plotly_chart(fig)
+    
+
+
+
+
+# retreieve default data 
+
+data = load_data()
+
+
+
+
+
+st.title("Stock Market Dashboard")
+
 
 # SIDE BAR
 
 with st.sidebar:
-    st.title("YouTube Channel Dashboard")
-    st.header("⚙️ Settings")
+    st.header("Settings")
     
+    min_date =  data.index.min().date()
     max_date = data.index.max().date()
-    default_start_date = max_date - timedelta(days=365)  # Show a year by default
-    default_end_date = max_date
-    start_date = st.date_input("Start date", default_start_date, min_value=data.index.min().date(), max_value=max_date)
-    end_date = st.date_input("End date", default_end_date, min_value=data.index.min().date(), max_value=max_date)
+    
+    start_date = st.date_input("Start date", min_date, min_value=min_date, max_value=max_date)
+    end_date = st.date_input("End date", max_date, min_value=min_date, max_value=max_date)
     time_frame = st.selectbox("Select time frame",
                               ("Daily", "Weekly", "Monthly", "Quarterly"),
     )
     chart_selection = st.selectbox("Select a chart type",
                                    ("Bar", "Area"))
+    
+    symbol = st.selectbox("Select ticker symbol",
+                                   get_tickers_name())
+    
 
-# VISUALIZATIONS
-# st.subheader("Closing Price")
-# st.line_chart(data['Adj Close'])
+data = load_data(start_date, end_date, symbol)
 
-# st.subheader("Volume Price")
-# st.line_chart(data['Volume'])
+
+display_line_graph(symbol, data)
+display_candle_stick(symbol, data)
+
+# get the fundamental data about the ticker performance today
+columns = ["previousClose", "open", "dayLow", "dayHigh"]
+
+for col in columns:
+    with st.container(border=True):
+        st.write(ticker_object.info[col])
+    
+
+# SEEING data and exporting option to csv file
+with st.expander('View Original Data'):
+    st.dataframe(data)
+    
+    # st.download_button("Download Ticker Fundamental Data", data.to_csv(index=True), file_name=f"{symbol}_fundamental_data.csv", mime="text/csv")
+    st.download_button("Download Historical Stock Data", data.to_csv(index=True), file_name=f"{symbol}_stock_data.csv", mime="text/csv")
